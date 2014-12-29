355ab2345c77c52bdc6cccfbdbf49c6777a15568
Mattan Ingram <mattaningram@gmail.com>
2014-12-29 00:10:38 -0500

    set up initial gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,21 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    livereload = require('gulp-livereload');
+
+gulp.task('sass', function () {
+    gulp.src('app/styles/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('build/styles'))
+        .pipe(livereload());
+});
+
+gulp.task('reload', function () {
+    gulp.src('app/**/*.{html,js}')
+        .pipe(livereload());
+});
 
 gulp.task('default', function() {
-  // place code for your default task here
+    livereload.listen();
+    gulp.watch('app/styles/scss/*.scss', ['sass']);
+    gulp.watch('app/**/*.{html,js}', ['reload']);
 });
